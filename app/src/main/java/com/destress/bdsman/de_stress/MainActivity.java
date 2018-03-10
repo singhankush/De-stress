8848ddc52ae10d37a053679c769bf5bcd7462429
Ankush Singh <ankush.1996@gmail.com>
2018-03-10 17:52:33 +0530

    Layout set

--- app/src/main/java/com/destress/bdsman/de_stress/MainActivity.java
+++ app/src/main/java/com/destress/bdsman/de_stress/MainActivity.java
@@ -1,20 +1,30 @@
 package com.destress.bdsman.de_stress;
 
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Surface;
+import android.view.SurfaceView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.HorizontalScrollView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private HorizontalScrollView mListView;
-    private View mSnapView;
+    private HorizontalScrollView mScrollView;
+    private SurfaceView mSnapView;
+    private Camera mCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mListView = findViewById(R.id.list);
+        mScrollView = findViewById(R.id.scroll_view);
+        mSnapView = findViewById(R.id.snap_view);
+
+    }
+
+    private void safeOpenCamera(View view){
 
     }
 }
