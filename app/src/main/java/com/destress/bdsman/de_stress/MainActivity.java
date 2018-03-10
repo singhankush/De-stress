441b0d4b4d0e5321dcf6d00d8c194bcee662c9c8
vipul <vipul26.h@gmail.com>
2018-03-10 17:54:49 +0530

    Default Audio Play Added on Main Activity Open.

--- app/src/main/java/com/destress/bdsman/de_stress/MainActivity.java
+++ app/src/main/java/com/destress/bdsman/de_stress/MainActivity.java
@@ -1,20 +1,51 @@
 package com.destress.bdsman.de_stress;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.HorizontalScrollView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     private HorizontalScrollView mListView;
     private View mSnapView;
 
+    //Declaration of Package Name
+    //Used to retrieve Audio Uri
+    public String PACKAGE_NAME;
+
+    // Variable for Audio Player
+    private MediaPlayer audioPlayer;
+    private Uri audioUri;
+    private int defaultAudioId;
+    //End of Variable for Audio Player
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        PACKAGE_NAME = getApplicationContext().getPackageName();
         mListView = findViewById(R.id.list);
 
+        //Audio Player Work Started
+        audioPlayer = new MediaPlayer();
+        defaultAudioId = R.raw.owms; //default mp3 audio.
+        audioUri = getAudioUri(defaultAudioId);
+        try{
+            audioPlayer.setDataSource(getApplicationContext(), audioUri);
+        }catch (IOException e){
+            Log.d("ErrorMessage","Audio Player Unable to get Audio Uri or Context");
+        }
+        //Audio Player Ended
+    }
+
+    //Converts Raw data id to Uri for Audio Player
+    public Uri getAudioUri(int id){
+        return Uri.parse("android.resource://" + PACKAGE_NAME + "/" + id);
     }
 }
